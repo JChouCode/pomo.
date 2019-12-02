00eafdb1505ef27512f34bb75f94e036d0f5bb11
Jeffrey <jc2573@cornell.edu>
2019-12-02 17:03:38 -0500

    Deploy files

---
+++ precache-manifest.e827b677acd1d95fca88ad199e33c491.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf376ce22355c48d7a371bcf8127dbc4",
+    "url": "/pomo./index.html"
+  },
+  {
+    "revision": "3b0230a96b7035d99eee",
+    "url": "/pomo./static/css/main.d778eb4e.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/pomo./static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "3b0230a96b7035d99eee",
+    "url": "/pomo./static/js/main.7c02a14e.chunk.js"
+  },
+  {
+    "revision": "9bb200b4986fe83b7361",
+    "url": "/pomo./static/js/runtime~main.ed33c73a.js"
+  }
+]);
